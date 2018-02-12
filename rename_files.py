ed766969e1c64845a30dd229d0972db30c0f3792
Alan Bridgeman <alan@wpa-1-444.cc.umanitoba.ca>
2018-02-12 14:26:58 -0600

    Updated Python script to extract 035 from MarcXML

--- rename_files.py
+++ rename_files.py
@@ -1,5 +1,48 @@
-from ConfigParser import SafeConfigParser
+try:
+  from lxml import etree
+  print("running with lxml.etree")
+except ImportError:
+  try:
+    # Python 2.5
+    import xml.etree.cElementTree as etree
+    print("running with cElementTree on Python 2.5+")
+  except ImportError:
+    try:
+      # Python 2.5
+      import xml.etree.ElementTree as etree
+      print("running with ElementTree on Python 2.5+")
+    except ImportError:
+      try:
+        # normal cElementTree install
+        import cElementTree as etree
+        print("running with cElementTree")
+      except ImportError:
+        try:
+          # normal ElementTree install
+          import elementtree.ElementTree as etree
+          print("running with ElementTree")
+        except ImportError:
+          print("Failed to import ElementTree from any known place")
 
-parser = SafeConfigParser()
-parser.read('The Legacy-metadata.ini')
-print parser.get('The Legacy', 'title')
+from ConfigParser import SafeConfigParser
+import subprocess
+
+args = ['find', '.', '-name', '*.xml'] #, '-print0']
+proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+stdout, stderr = proc.communicate()
+lines = stdout.decode('ascii').splitlines()
+for line in lines:
+	with035 = {}
+	
+	print "Parsing " + line
+	tree = etree.parse(line)
+	root = tree.getroot()
+	for record in root:
+		for field in record:
+			if field.get("tag") == '035':
+				for code in field:
+					with035[code.text] = record
+	print with035
+#parser = SafeConfigParser()
+#parser.read('The Legacy-metadata.ini')
+#print parser.get('The Legacy', 'title')
