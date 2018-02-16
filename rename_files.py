b6ab1121d0d76694a82823bf643dd047f08b794e
Alan Bridgeman <alan@Alans-MacBook-Pro.local>
2018-02-16 11:46:57 -0600

    Added a bunch of framework srtudf for filerenameing

--- rename_files.py
+++ rename_files.py
@@ -1,15 +1,24 @@
 import click
+import subprocess
 
 bySCN = {}
 byTitle = {}
 
 def proc_get_title():
 	input = click.prompt('What is the SCN of the title your looking for?')
-	title = bySCN[input]
+	try:
+		title = bySCN[input]
+	except:
+		title = 'Unavailable (No Such Entry)'
 	click.echo('The title of the book is: ' + title)
 
 def proc_get_SCN():
-	click.echo('Getting SCN')
+	input = click.prompt('What is the title of the book your looking foer?')
+	try:
+		SCN = byTitle[input]
+	except:
+		SCN = 'Unavailable (No Such Entry)'
+	click.echo('The SCN of the book is: ' + SCN)
 
 def proc_get():
 	options = ['title', 'SCN']
@@ -35,9 +44,32 @@ def proc_set():
         input = click.prompt('What would you like to get?', type=click.Choice(options))
         globals()[callbacks[input]]()
 
+def rename(pattern):
+	args = ['find', '.', '-name', pattern]
+	proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+	stdout, stderr = proc.communicate()
+	lines = stdout.decode('ascii').splitlines()
+	for line in lines:
+		if '.' in line:
+			parts = line.split('.')
+			if len(parts[0]) < 1:
+				parts.remove(parts[0])
+			if len(parts) == 2:
+				if len(parts[0]) > 0:
+					name = parts[0]
+					if '_' in name:
+						tokens = name.split('_')
+						print tokens
+					else:
+						print 'To be implemented...'
+					format = parts[1]
+			else:
+				print 'There is something odd about ' + line + 'because broken down it changes into' + str(parts)
+
 @click.command()
 @click.option('--dictionary', prompt='Dictionary', help='The filename that you want to be processed')
-def main(dictionary):
+@click.option('--pattern', prompt='File pattern', help='The file pattern to use for looking up relavent filenames')
+def main(dictionary, pattern):
 	click.echo('Opening %s' % file)
 	with open(dictionary) as f:
 		lines = f.readlines()
@@ -49,11 +81,14 @@ def main(dictionary):
 		bySCN[tokens[0]] = tokens[1]
 		byTitle[tokens[1]] = tokens[0]
 	
+	rename(pattern)
+	
 	options = ['get', 'set']
 	callbacks = {}
 	for option in options:
 		callbacks[option] = 'proc_' + option
 	input = click.prompt('What would you like to do?', type=click.Choice(options))
 	globals()[callbacks[input]]()
+
 if __name__ == '__main__':
 	main()
